e8b33042a1c9c566f416bdaa24048aa623e4bfa6
Gustavo R. Vargas <119086059+Gustavo-Vargas@users.noreply.github.com>
2023-08-22 20:21:31 -0600

    Creacion de ventana Principal
    Se creo la ventana principal con un menubar y dos menu itmes (Abrir y cerrar)

---
+++ NyxCode/src/main/java/com/nyxcode/nyxcode/NyxCode.java
@@ -0,0 +1,15 @@
+package com.nyxcode.nyxcode;
+
+import com.nyxcode.nyxcode.views.Principal;
+
+/**
+ *
+ * @author DocSe
+ */
+public class NyxCode {
+
+    public static void main(String[] args) {
+        Principal v = new Principal();
+        v.setVisible(true);
+    }
+}

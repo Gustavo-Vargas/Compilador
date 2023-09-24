579ae036338189c0855f4bccbd2d78825880f5e6
Gustavo R. Vargas <119086059+Gustavo-Vargas@users.noreply.github.com>
2023-09-23 22:56:44 -0600

    se agrego hasMap de lexemas

--- NyxCode/src/main/java/com/nyxcode/compilador/controls/Lexema.java
+++ NyxCode/src/main/java/com/nyxcode/compilador/controls/Lexema.java
@@ -5,6 +5,8 @@
 package com.nyxcode.compilador.controls;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -16,22 +18,22 @@ public class Lexema {
     private int token;
     private short linea;
 
-    private ArrayList<String> reservadas;
+    private Map<Integer, String> lstLex;
 
     public Lexema(String valor, short linea) {
         this.valor = valor;
         this.linea = linea;
-        reservadas.add("const");
-        reservadas.add("var");
-        reservadas.add("proced");
+        listLexemas();
         token = selectToken(valor);
     }
 
     private int selectToken(String palabra) {
         int t = 0;
 
-        if (reservadas.contains(palabra.toLowerCase())) {
-            return reservadas.indexOf(palabra.toLowerCase());
+        if (lstLex.containsValue(valor.toLowerCase())) {
+            //return lstLex.ge
+
+            //return lstLex.indexOf(palabra.toLowerCase());
         }
 
         try {
@@ -41,7 +43,54 @@ public class Lexema {
         } catch (NumberFormatException e) {
 
         }
+
         return t;
+
+    }
+
+    private void listLexemas() {
+        lstLex = new TreeMap<Integer, String>();
+        // Palabras Reservadas
+        lstLex.put(1, "Const");
+        lstLex.put(2, "Var");
+        lstLex.put(3, "Proced");
+        lstLex.put(4, "Begin");
+        lstLex.put(5, "Write");
+        lstLex.put(6, "Read");
+        lstLex.put(7, "Call");
+        lstLex.put(8, "If");
+        lstLex.put(9, "While");
+        lstLex.put(10, "Then");
+        lstLex.put(11, "Do");
+        lstLex.put(12, "For");
+        lstLex.put(13, "To");
+        lstLex.put(14, "Dto");
+        lstLex.put(15, "End");
+
+        // Operadores
+        lstLex.put(20, "+");
+        lstLex.put(21, "-");
+        lstLex.put(22, "*");
+        lstLex.put(23, "/");
+
+        lstLex.put(30, "=");
+        lstLex.put(31, "==");
+        lstLex.put(32, "!=");
+        lstLex.put(33, "<");
+        lstLex.put(34, "<=");
+        lstLex.put(35, ">");
+        lstLex.put(36, ">=");
+
+        // Separadpres
+        lstLex.put(40, ".");
+        lstLex.put(41, ",");
+        lstLex.put(42, ";");
+        lstLex.put(43, "(");
+        lstLex.put(44, ")");
+
+        // identificadores y Numeros
+        lstLex.put(50, "id");
+        lstLex.put(60, "num");
 
     }
 
